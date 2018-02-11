8d15d503cb8f911c4aae3a6927919df9736e5298
Rachel Pavlakovic <rachelpav@comcast.net>
2018-02-11 15:45:10 -0500

    Add files via upload
    M_state_if

--- Interpreter.rkt
+++ Interpreter.rkt
@@ -9,11 +9,22 @@
   (lambda (fileName)
     ()))
 
-(define M_state_if)
+(define M_state_if
+  (lambda (cond then else state)
+    (if (M_bool cond state)
+        (M_state_stmt then state)
+        (M_state_stmt else state))))
+   
 (define M_state_while)
 (define M_state_assign)
 (define M_state_return)
 (define M_state_var)
+(define M_state_expr)
+(define M_state_stmt)
 
-(define M_value)
+(define M_value_var)
+(define M_value_assign)
+(define M_value_return)
+(define M_value_expr)
+
 (define M_bool)
