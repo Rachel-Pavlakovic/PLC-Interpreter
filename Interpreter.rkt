7be1444da27fd18e1cc75fa5c91d707e3ad2c53a
Rachel Pavlakovic <rachelpav@comcast.net>
2018-02-11 15:51:25 -0500

    Add files via upload

--- Interpreter.rkt
+++ Interpreter.rkt
@@ -9,6 +9,8 @@
   (lambda (fileName)
     ()))
 
+(define M_state)
+
 (define M_state_if
   (lambda (cond then else state)
     (if (M_bool cond state)
@@ -22,9 +24,28 @@
 (define M_state_expr)
 (define M_state_stmt)
 
+(define M_value)
+
 (define M_value_var)
 (define M_value_assign)
 (define M_value_return)
 (define M_value_expr)
+(define M_value_int
+  (lambda (e)
+    (cond
+      ((number? e) e)
+      ((eq? '+ (operator e)) (+ (M_value_int (operand1 e)) (M_value_int (operand2 e))))
+      ((eq? '- (operator e)) (- (M_value_int (operand1 e)) (M_value_int (operand2 e))))
+      ((eq? '* (operator e)) (* (M_value_int (operand1 e)) (M_value_int (operand2 e))))
+      ((eq? '/ (operator e)) (quotient (M_value_int (operand1 e)) (M_value_int (operand2 e))))
+      ((eq? '% (operator e)) (remainder (M_value_int (operand1 e)) (M_value_int (operand2 e))))
+      (else (error 'badop "Undefine operator")))))
+
+(define operator
+  (lambda (e)
+    (cadr e)))
+
+(define operand1 car)
+(define operand2 caddr)
 
 (define M_bool)
